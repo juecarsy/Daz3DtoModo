a9da02ccc0812ab417e4595a220374af2c0ef9bf
juecarsy <31792164+juecarsy@users.noreply.github.com>
2018-10-03 15:05:14 +0800

    Add files via upload

---
+++ bone_fit_fix.py
@@ -0,0 +1,67 @@
+#python
+import lx
+import copy
+
+itemNum = lx.eval("query sceneservice item.N ? ");
+itemNumList = range(0, itemNum);
+
+itemList = {};
+
+for item in itemNumList:
+	itemID = lx.eval("query sceneservice item.id ? %s" %item);
+	itemName = lx.eval("query sceneservice item.name ? %s" %item);
+	itemList[item] = itemID + ' ' + itemName;
+#itemList.append(itemID);
+lx.out(itemList);
+
+
+selItems = lx.evalN("query sceneservice selection ? all")
+
+
+lis = list(selItems) #tuple to list conversion
+lx.out(selItems) #tuple
+lx.out(lis) #list
+
+cha = []
+for item in lis:
+	lx.eval("select.subitem %s add mesh 1 1" %item)
+	newSel =lx.eval("query sceneservice selection ? all")
+	for element in newSel:
+		if element not in cha:
+			cha.append(element)
+	
+chaName =[]	
+lx.out(cha)
+for item in cha:
+	itemName = lx.eval("query sceneservice item.name ? %s" %item)
+	lx.out(itemName)
+	chaName.append(itemName)
+lx.out(chaName)	
+for item in cha:
+	lx.eval("select.subitem %s remove mesh 1 1" %item)
+lx.out(cha)
+chm = copy.copy(cha)
+lx.out(chm)
+for a in cha:
+	aName = lx.eval("query sceneservice item.name ? %s" %a)
+	chm.remove(a)
+	#lx.out(chm)
+	lx.out(aName)
+	for b in chm:
+		bName = lx.eval("query sceneservice item.name ? %s" %b)
+		if str(bName) + '_2' == str(aName): 
+			
+			lx.eval("select.subitem %s set mesh 0 0" %b)
+			lx.eval("select.subitem %s add mesh 0 0" %a)
+			lx.eval("constraintTransform add pos false")
+
+    
+
+
+
+
+
+	 
+  #lx.out(chName)	#aha.append(ha);
+	#lx.out(ha);
+#lx.out(aha)
