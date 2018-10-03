24b561863f9885fbaf856ce14cf74b4b30c9f525
juecarsy <31792164+juecarsy@users.noreply.github.com>
2018-10-03 15:39:54 +0800

    fixed some visibility problems

--- bone_fit_fix.py
+++ bone_fit_fix.py
@@ -23,6 +23,7 @@ lx.out(selItems) #tuple
 lx.out(lis) #list
 
 cha = []
+
 for item in lis:
 	lx.eval("select.subitem %s add mesh 1 1" %item)
 	newSel =lx.eval("query sceneservice selection ? all")
@@ -41,6 +42,7 @@ for item in cha:
 	lx.eval("select.subitem %s remove mesh 1 1" %item)
 lx.out(cha)
 chm = copy.copy(cha)
+vcha = copy.copy(cha)
 lx.out(chm)
 for a in cha:
 	aName = lx.eval("query sceneservice item.name ? %s" %a)
@@ -51,9 +53,16 @@ for a in cha:
 		bName = lx.eval("query sceneservice item.name ? %s" %b)
 		if str(bName) + '_2' == str(aName): 
 			
-			lx.eval("select.subitem %s set mesh 0 0" %b)
-			lx.eval("select.subitem %s add mesh 0 0" %a)
+			lx.eval("select.subitem %s set mesh " %a)
+			lx.eval("select.subitem %s add mesh " %b)
 			lx.eval("constraintTransform add pos false")
+
+for item in vcha:
+	lx.eval("layer.setVisibility %s true" %item)
+lx.out(vcha)
+
+
+
 
     
 
@@ -65,3 +74,4 @@ for a in cha:
   #lx.out(chName)	#aha.append(ha);
 	#lx.out(ha);
 #lx.out(aha)
+
